27989a756b421cc4c173b28f5f0d0f98fd5b598a
veev13 <veev13@gmail.com>
2021-02-03 16:54:36 +0900

    exam

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const app = express();
 const port = process.env.PORT|| 3000;
 
 app.get('/', (req, res)=>res.send('hello, Nodejs. /'));
-app.get('/bye', (req, res)=>res.send('bye, Nodejs'));
-app.get('/welcome', (req, res)=>{
-    var user_name = req.param('name');
-    res.send('Hi, Welcome to the NodeJs Service ['+user_name+']')
+app.get('/service', (req, res)=>{
+    var status = req.param('status');
+    res.send('The system is running [Status: '+ status.toUpperCase()+']')
 });
 app.listen(port, ()=> console.log('example app listening on port 3000'));
